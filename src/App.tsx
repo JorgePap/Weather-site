588bec9745c64da6b318d9eb9aa2e0a70098e2ef
JorgePap <giwrgos.p1997@gmail.com>
2024-07-28 17:57:39 +0300

    deploy #5

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,14 @@
 import "./App.css";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 import { Routing } from "./router/Routing";
 // import { NavBar } from "./ui/lib";
 
 function App() {
   return (
-    <BrowserRouter>
+    <HashRouter>
       {/* <NavBar /> */}
       <Routing />
-    </BrowserRouter>
+    </HashRouter>
   );
 }
 
